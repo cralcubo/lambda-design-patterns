dc91eac1ec8c0599a3160d756a38131f15f757c3
Christian Roman <cralcubo@hotmail.com>
2018-01-08 21:25:26 +0100

    Added the Visitor pattern

---
+++ src/main/java/com/chris/lambda/patterns/VisitorLambda.java
@@ -0,0 +1,66 @@
+package com.chris.lambda.patterns;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class VisitorLambda {
+
+	public static class VisitorFunction<A> implements Function<Object, A> {
+		private Map<Class<?>, Function<Object, A>> functionMap = new HashMap<>();
+
+		public <B> Acceptor<A, B> on(Class<B> clazz) {
+			return new Acceptor<>(this, clazz);
+		}
+
+		@Override
+		public A apply(Object o) {
+			return functionMap.get(o.getClass()).apply(o);
+		}
+
+		public static class Acceptor<A, B> {
+			private final VisitorFunction<A> visitor;
+			private final Class<B> clazz;
+
+			public Acceptor(VisitorFunction<A> visitor, Class<B> clazz) {
+				this.visitor = visitor;
+				this.clazz = clazz;
+			}
+
+			public VisitorFunction<A> then(Function<B,A> f) {
+				visitor.functionMap.put(clazz, (Function<Object, A>) f);
+				return visitor;
+			}
+		}
+
+	}
+
+	public static class Square {
+		final double side;
+
+		public Square(double s) {
+			this.side = s;
+		}
+	}
+
+	public static class Circle {
+		final double radio;
+
+		public Circle(double r) {
+			this.radio = r;
+		}
+	}
+	
+	static Function<Object, Double> area = new VisitorFunction<Double>().on(Square.class).then(s -> s.side * s.side)
+																	   .on(Circle.class).then(c -> c.radio * c.radio * Math.PI);
+	
+	public static void main(String[] args) {
+		System.out.println(
+		area.apply(new Square(10)));
+		
+		System.out.println(
+				area.apply(new Circle(10)));
+		
+	}
+
+}
